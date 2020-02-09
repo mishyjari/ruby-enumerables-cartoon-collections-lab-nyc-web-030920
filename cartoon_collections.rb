b6120c3740cc37b24ae5ab2ef0a232f8f488de0f
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-09 03:36:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls)
   calls.any? { | word | word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.
 end
