f8ca075990dde4588914d77b749f84192396d9c9
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-09 03:43:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
   foods.each { | item | 
-    if cheese_types.include? item
+    if cheese_types.include?(item)
       return item
     else 
       return nil
