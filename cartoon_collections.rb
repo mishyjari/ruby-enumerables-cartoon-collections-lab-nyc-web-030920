c5c894d050c855367db25734552f035384f7f63a
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-09 03:34:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(calls)
   calls.map { | call | "#{call.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.reduce
 end
 
 def find_the_cheese# code an argument here
